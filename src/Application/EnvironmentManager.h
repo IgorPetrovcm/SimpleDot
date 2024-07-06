4e5b89da97bea5cc8ca12a795dca41f4e221795b
IgorPetrovcm <igorpetrov42ru@gmail.com>
2024-07-06 23:49:18 +0300

    Update environment manager module

--- src/Application/EnvironmentManager.h
+++ src/Application/EnvironmentManager.h
@@ -2,15 +2,36 @@
 # define ENVIRONMENT_MANAGER
 
 # include "Mapping.h"
+# include "ProcessesManager.h"
 # include "../Model/AppSettings.h"
+
+# define SMD_LOGGING_ERROR "SIMPLEDOT_LOGGING_ERROR"
+# define SMD_LOGGING_INFO "SIMPLEDOT_LOGGING_INFO"
+# define SMD_LOGGING_WARNING "SIMPLEDOT_LOGGING_WARNING"
+
+# define LOGGING_ERROR_MODE 0
+# define LOGGING_INFO_MODE 1
+# define LOGGING_WARNING_MODE 2
 
 typedef struct {
     Mapping mapping;
+    ProcessesManager procMngr;
+    int outPipe[2];
+    int errPipe[2];
     char* (*get_variables)(void*, AppSettings*);
+    int (*push_settings_from_file)(char*);
+    char* (*get_logging_variable)(int);
+    int (*write_default_settings_to_file)(void*, AppSettings*, char*);
 } EnvironmentManager;
 
 char* get_variables(void* self, AppSettings* appSettings);
 
-EnvironmentManager constructor_environment_manager(Mapping mapping);
+int push_settings_from_file(char* pathToAppSettingsFile);
+
+char* get_logging_variable(int mode);
+
+int write_default_settings_to_file(void* self, AppSettings* appSettings, char* pathToAppSettingsFile);
+
+EnvironmentManager constructor_environment_manager(Mapping mapping, ProcessesManager procMngr);
 
 # endif
--- src/Service/EnvironmentManager.c
+++ src/Service/EnvironmentManager.c
@@ -2,24 +2,27 @@
 # include "../Application/Mapping.h"
 # include "../Model/AppSettings.h"
 # include <string.h>
+# include <stdio.h>
 # include <stdlib.h>
+# include <errno.h>
 
-# define ENV_SMD "SIMPLEDOT_"
-# define ENV_LOGGING "LOGGING_"
-# define ENV_ERROR "ERROR"
-# define ENV_INFO "INFO"
-# define ENV_WARNING "WARNING"
+char* const logging_modes[3] = { SMD_LOGGING_ERROR, SMD_LOGGING_INFO, SMD_LOGGING_WARNING };
 
-EnvironmentManager constructor_environment_manager(Mapping mapping)
+
+EnvironmentManager constructor_environment_manager(Mapping mapping, ProcessesManager procMngr)
 {
     EnvironmentManager envMngr;
 
     envMngr.mapping = mapping;
+    envMngr.procMngr = procMngr;
+
     envMngr.get_variables = get_variables;
+    envMngr.push_settings_from_file = push_settings_from_file;
+    envMngr.get_logging_variable = get_logging_variable;
+    envMngr.write_default_settings_to_file = write_default_settings_to_file;
 
     return envMngr;
 }
-
 
 char* get_variables(void* self, AppSettings* appSettings)
 {
@@ -31,9 +34,72 @@ char* get_variables(void* self, AppSettings* appSettings)
     char* infoStrDTO = envMngr->mapping.to_dto(appSettings->loggingSettings.info);
     char* warningStrDTO = envMngr->mapping.to_dto(appSettings->loggingSettings.warning);
 
-    strcpy(result, envMngr->mapping.get_appsetting_string(WITH_N, errorStrDTO, 3, ENV_SMD, ENV_LOGGING, ENV_ERROR));
-    strcat(result, envMngr->mapping.get_appsetting_string(WITH_N, infoStrDTO, 3, ENV_SMD, ENV_LOGGING, ENV_INFO));
-    strcat(result, envMngr->mapping.get_appsetting_string(WITH_N, warningStrDTO, 3, ENV_SMD, ENV_LOGGING, ENV_WARNING));
+    strcpy(result, envMngr->mapping.get_appsetting_string(WITH_N, errorStrDTO, logging_modes[0]));
+    strcat(result, envMngr->mapping.get_appsetting_string(WITH_N, infoStrDTO, logging_modes[1]));
+    strcat(result, envMngr->mapping.get_appsetting_string(WITH_N, warningStrDTO, logging_modes[2]));
     
     return result;
+}
+
+int write_default_settings_to_file(void *self, AppSettings* appSettings, char* pathToAppSettingsFile)
+{
+    errno = 0;
+    int errNum;
+
+    EnvironmentManager* envMngr = (EnvironmentManager*)self; 
+
+    char* settings = envMngr->get_variables(envMngr, appSettings);
+
+    FILE* appSettingsFile = fopen(pathToAppSettingsFile, "w+");
+
+    if ((errNum = errno) != 0){
+        return errNum;
+    }
+
+    fwrite(settings, sizeof(char), strlen(settings), appSettingsFile);
+
+    if ((errNum = errno) != 0){
+        char* buffer = strerror(errNum);
+        fprintf(stderr, "%s", buffer);
+    }
+
+    fclose(appSettingsFile);
+
+    return 0;
+}
+
+int push_settings_from_file(char* pathToAppSettingsFile)
+{
+    errno = 0;
+    int errNum;
+
+    char lineBuffer[256];
+
+    FILE* appSettingsFile = fopen(pathToAppSettingsFile, "r");
+
+    if ((errNum = errno) != 0){
+        char* errStr = strerror(errNum);
+        fprintf(stderr, "%s", errStr);
+    }
+
+
+    while (fgets(lineBuffer, sizeof(lineBuffer), appSettingsFile) != NULL){
+        char* delim = "=";
+        char* envName = strtok(lineBuffer, delim);
+        char* envValue = strtok(NULL, delim);
+
+        setenv(envName, envValue, 1);
+    }
+
+    if ((errNum = errno) != 0){
+        char* errStr = strerror(errNum);
+        fprintf(stderr, "%s", errStr);
+    }
+
+    fclose(appSettingsFile);
+}
+
+char* get_logging_variable(int mode)
+{
+    return getenv(logging_modes[mode]);
 }
