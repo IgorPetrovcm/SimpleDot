44b6a574fbc598c567148869d934e87f3a538d9f
IgorPetrovcm <igorpetrov42ru@gmail.com>
2024-07-10 18:39:59 +0300

    Update PackageWorker logic

--- src/Application/PackageWorker.h
+++ src/Application/PackageWorker.h
@@ -3,19 +3,31 @@
 
 # include "ProcessesManager.h" 
 
+# define CHECK_PACKAGE 0
+# define INSTALL_PACKAGE 1
+# define UNISTALL_PACKAGE 2
+
 typedef struct {
     int outPipe[2];
     int errPipe[2];
 
     ProcessesManager* processesManager;
 
-    // int (*open_std_pipes) (void*);
-    int (*is_package_exists) (void*, char*);
+
+    char* (*get_check_str)(void* self, char* packageName);
+    char* (*get_install_str)(void* self, char* packageName);
+    char* (*get_unistall_str)(void* self, char* packageName);
+
+    int (*launch_package_manager)(void* self, int mode, char* packageName);
 } PackageWorker;
 
-int is_package_exists(void* self, char* packageName);
+char* get_check_str(void* self, char* packageName);
 
-// int open_std_pipes(void* self);
+char* get_install_str(void* self, char* packageName);
+
+char* get_unistall_str(void* self, char* packageName);
+
+int launch_package_manager(void* self, int mode, char* packageName);
 
 PackageWorker* constructor_package_worker(ProcessesManager* processesManager);
 
--- src/Service/PackageWorker.c
+++ src/Service/PackageWorker.c
@@ -1,4 +1,5 @@
 # include "../Application/PackageWorker.h"
+#include <time.h>
 # include <unistd.h>
 # include <stdio.h>
 # include <stdlib.h>
@@ -9,6 +10,11 @@
 static const char* sudo = "sudo";
 static const char* package_manager = "pacman";
 static const char* q_flag = "-Q";
+static const char* s_flag = "-S";
+static const char* unis_flag = "-Runs";
+static const char* no_confirm_flag = "--noconfirm";
+
+char* (*get_options[3])(void* self, char* packageName) = {get_check_str, get_install_str, get_unistall_str};
 
 PackageWorker* constructor_package_worker(ProcessesManager* processesManager)
 {
@@ -16,72 +22,60 @@ PackageWorker* constructor_package_worker(ProcessesManager* processesManager)
 
     packageWorker->processesManager = processesManager;
 
-    packageWorker->is_package_exists = is_package_exists;
+    packageWorker->get_check_str = get_check_str;
+    packageWorker->get_install_str = get_install_str;
+    packageWorker->get_unistall_str = get_unistall_str;
+    packageWorker->launch_package_manager = launch_package_manager;
 
     return packageWorker;
 }
 
-int is_package_exists(void* self, char* packageName)
+char* get_check_str(void* self, char* packageName)
+{
+    PackageWorker* packageWorker = (PackageWorker*)self;
+
+    char* options = malloc(256);
+
+    packageWorker->processesManager->adding_to_buffer(options, 5, package_manager, " ", q_flag, "i ", packageName);
+
+    return options;
+}
+
+char* get_install_str(void* self, char* packageName)
+{
+    PackageWorker* packageWorker = (PackageWorker*)self;
+
+    char* options = malloc(256);
+
+    packageWorker->processesManager->adding_to_buffer(options, 9, sudo, " ", package_manager, " ", s_flag, " ", no_confirm_flag, " ", packageName);
+
+    return options;
+}
+
+char* get_unistall_str(void* self, char* packageName)
+{
+    PackageWorker* packageWorker = (PackageWorker*)self;
+
+    char* options = malloc(256);
+
+    packageWorker->processesManager->adding_to_buffer(options, 9, sudo, " ", package_manager, " ", unis_flag, " ", no_confirm_flag, " ", packageName);
+
+    return options;
+}
+
+int launch_package_manager(void* self, int mode, char* packageName)
 {
     if (packageName == NULL){
         return -1;
     }
 
-    errno = 0;
-    int errNum = 0;
-
     PackageWorker* packageWorker = (PackageWorker*)self;
 
-    if ((errNum = packageWorker->processesManager->open_pipes(2, packageWorker->outPipe, packageWorker->errPipe)) != 0){
-        return errNum;
+    char* options = get_options[mode](packageWorker, packageName);
+
+    if (packageWorker->processesManager->write_process(packageWorker->processesManager, options) != 0){
+        return -1;
     }
 
-    pid_t childProcessPID = fork();
-
-    char options[100];
-
-    strcpy(options, package_manager);
-    strcat(options, " ");
-    strcat(options, q_flag);
-    strcat(options, "i ");
-    strcat(options, packageName);
-
-    if (childProcessPID == -1){
-        errNum = errno;
-        return errNum;
-    }
-    else if (childProcessPID == 0){
-        int copyOfOutFileDescriptorID = packageWorker->outPipe[1];
-        int copyOfErrFileDescriptorID = packageWorker->errPipe[1];
-
-        dup2(copyOfOutFileDescriptorID, STDOUT_FILENO);
-        dup2(copyOfErrFileDescriptorID, STDERR_FILENO);
-
-        execl("/bin/sh", "sh", "-c", options);
-
-        packageWorker->processesManager->close_fds(2, STDOUT_FILENO, STDERR_FILENO);
-    }
-    else {
-        int childProcessStatus;
-        waitpid(childProcessPID, &childProcessStatus, 0);
-
-        if (!WIFEXITED(childProcessStatus)){
-            errNum = WEXITSTATUS(childProcessPID);
-            return errNum;
-        }
-
-        packageWorker->processesManager->close_fds(2, packageWorker->errPipe[1], packageWorker->outPipe[1]);
-
-        int countReadErrSymbols = 0;
-        char ch;
-        while (read(packageWorker->errPipe[0], &ch, 1) > 0){
-            countReadErrSymbols++;
-        }   
-
-        if (countReadErrSymbols > 0){
-            return -1;
-        }
-
-        return 0;
-    }
+    return 0;
 }
