20d0ba583d21945ba2e3945519d581992285b885
IgorPetrovcm <igorpetrov42ru@gmail.com>
2024-07-10 20:08:15 +0300

    Update Tests for PackageWorker

--- src/Test/PackageWorkerTesting.c
+++ src/Test/PackageWorkerTesting.c
@@ -4,11 +4,13 @@
 # include "../Application/PackageWorker.h"
 # include <unistd.h>
 
-# define PACKAGE_EXISTS_CHECK_EXISTING_PACKAGE is_package_exists_check_existing_package_should_return_true
-# define PACKAGE_NOT_EXISTS_CHECK_EXISTING_PACKAGE is_package_exitsts_check_not_existsing_package_should_return_false
-# define PACKAGE_NAME_EQUAL_NELL_CHECK_EXISTING_PACKAGE is_package_existing_check_package_name_equal_null_should_return_false
-# define PACKAGE_NAME_EXISTS_INSTALL_PACKAGE package_install_install_exists_package_should_return_true
-# define PACKAGE_NAME_NOT_EXISTS_DOESNT_INSTALL_PACKAGE package_install_not_exists_package_return_false
+# define PACKAGE_EXISTS_CHECK_EXISTING_PACKAGE launch_package_manager_check_existing_package_should_return_true
+# define PACKAGE_NOT_EXISTS_CHECK_EXISTING_PACKAGE launch_package_manager_check_not_existsing_package_should_return_false
+# define PACKAGE_NAME_EQUAL_NELL_CHECK_EXISTING_PACKAGE launch_package_manager_check_package_name_equal_null_should_return_false
+# define PACKAGE_NAME_EXISTS_INSTALL_PACKAGE launch_package_manager_install_exists_package_should_return_true
+# define PACKAGE_NAME_NOT_EXISTS_DOESNT_INSTALL_PACKAGE launch_package_manager_not_exists_package_return_false
+# define PACKAGE_EXISTS_UNISTALL_PACKAGE launch_package_manager_unistall_exists_package_should_return_true
+# define PACKAGE_NOT_EXISTS_UNISTALL_PACKAGE launch_package_manager_unistall_not_exists_package
 
 static const char* structName = "PackageWorker";
 static const char* isPackageExistsFunctionName = "is_package_exists()";
@@ -18,6 +20,8 @@ void PACKAGE_NOT_EXISTS_CHECK_EXISTING_PACKAGE(Host* host);
 void PACKAGE_NAME_EQUAL_NELL_CHECK_EXISTING_PACKAGE(Host* host);
 void PACKAGE_NAME_EXISTS_INSTALL_PACKAGE(Host* host);
 void PACKAGE_NAME_NOT_EXISTS_DOESNT_INSTALL_PACKAGE(Host* host);
+void PACKAGE_EXISTS_UNISTALL_PACKAGE(Host* host);
+void PACKAGE_NOT_EXISTS_UNISTALL_PACKAGE(Host* host);
 
 int main(){
     Host* host = constructor_host();
@@ -37,6 +41,8 @@ int main(){
     PACKAGE_NAME_EQUAL_NELL_CHECK_EXISTING_PACKAGE(host);
     PACKAGE_NAME_EXISTS_INSTALL_PACKAGE(host);
     PACKAGE_NAME_NOT_EXISTS_DOESNT_INSTALL_PACKAGE(host);
+    PACKAGE_EXISTS_UNISTALL_PACKAGE(host);
+    PACKAGE_NOT_EXISTS_UNISTALL_PACKAGE(host);
 }
 
 void PACKAGE_EXISTS_CHECK_EXISTING_PACKAGE(Host* host)
@@ -80,10 +86,10 @@ void PACKAGE_NAME_EXISTS_INSTALL_PACKAGE(Host* host)
     PackageWorker* packageWorker = constructor_package_worker(host->processesManager);
 
     if (packageWorker->launch_package_manager(packageWorker, INSTALL_PACKAGE, "antiword") == 0){
-        host->logging->log(LOG_INF, "Package name \"git\" install", __FILE__, __LINE__, __TIME__);
+        host->logging->log(LOG_INF, "Package name \"antiword\" install", __FILE__, __LINE__, __TIME__);
     }
     else {
-        host->logging->log(LOG_ERR, "Package name \"git\" not install", __FILE__, __LINE__, __TIME__);
+        host->logging->log(LOG_ERR, "Package name \"antiword\" not install", __FILE__, __LINE__, __TIME__);
     }
 }
 
@@ -96,5 +102,29 @@ void PACKAGE_NAME_NOT_EXISTS_DOESNT_INSTALL_PACKAGE(Host* host)
     }
     else {
         host->logging->log(LOG_ERR, "Package name \"gite\" install", __FILE__, __LINE__, __TIME__);
+    }
+}
+
+void PACKAGE_EXISTS_UNISTALL_PACKAGE(Host* host)
+{
+    PackageWorker* packageWorker = constructor_package_worker(host->processesManager);
+
+    if (packageWorker->launch_package_manager(packageWorker, UNISTALL_PACKAGE, "antiword") == 0){
+        host->logging->log(LOG_INF, "Package name \"antiword\" unistall succesfully", __FILE__, __LINE__, __TIME__);
+    }
+    else {
+        host->logging->log(LOG_ERR, "Package name \"antiword\" doesn't unistall", __FILE__, __LINE__, __TIME__);
+    }
+}
+
+void PACKAGE_NOT_EXISTS_UNISTALL_PACKAGE(Host* host)
+{
+    PackageWorker* packageWorker = constructor_package_worker(host->processesManager);
+
+    if (packageWorker->launch_package_manager(packageWorker, UNISTALL_PACKAGE, "antiword") == -1){
+        host->logging->log(LOG_INF, "Package name \"antiword\" doesn't unistall", __FILE__, __LINE__, __TIME__);        
+    }
+    else {
+        host->logging->log(LOG_ERR, "Package name \"antiword\" unistall succesfully", __FILE__, __LINE__, __TIME__);
     }
 }
