a37a162dcefc00aa1a353b1ac861844596a40cdc
IgorPetrovcm <igorpetrov42ru@gmail.com>
2024-07-10 18:39:15 +0300

    Update ProcessesManager logic

--- src/Application/ProcessesManager.h
+++ src/Application/ProcessesManager.h
@@ -2,13 +2,19 @@
 # define PROCESSES_MANAGER
 
 typedef struct {
+    int (*adding_to_buffer)(char* buffer, int count, ...);
     int (*open_pipes)(int count, ...);
     int (*close_fds)(int count, ...);
+    int (*write_process)(void* self, char* options);
 } ProcessesManager;
+
+int adding_to_buffer(char* buffer, int count, ...);
 
 int open_pipes(int count, ...);
 
 int close_fds(int count, ...);
+
+int write_process(void* self, char* options);
 
 ProcessesManager* constructor_processes_manager();
 
